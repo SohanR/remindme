e27b202db628b302f3d9d8bfa079b3a3019671fc
sohanr <mizan.rahman66d@gmail.com>
2022-01-29 23:27:30 +0600

    dtp import

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+// import React, { useEffect, useState } from "react";
+// import axios from"axios";
+// import DateTimePicker from "react-date-timepicker";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>RemindMe</h1>
     </div>
   );
 }

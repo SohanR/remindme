cf9fbb954873d7ea8bab6fd583c968bc4ee488bd
sohanr <mizan.rahman66d@gmail.com>
2022-02-03 21:57:39 +0600

    homepage header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
+import React, { useState } from "react";
+// import axios from"axios";
+import DateTimePicker from "react-datetime-picker";
 import './App.css';
 
-// import React, { useEffect, useState } from "react";
-// import axios from"axios";
-// import DateTimePicker from "react-date-timepicker";
 
 function App() {
+
+  const [reminderMsg, setReminderMsg] = useState("");
+  const [ remindAt, setRemindAt ] = useState("");
+
+  // add reminder function
+  const addReminder = () => {
+    return null;
+  } 
+
   return (
     <div className="App">
-  <h1>RemindMe</h1>
+      <div className="homepage">
+        <div className="homepage_header">
+          <h1>Remind Me</h1>
+          <input type="text" placeholder="Reminder Notes Here... " value={reminderMsg} onChange={ e => setReminderMsg }  />
+          <DateTimePicker 
+            value={remindAt}
+            onchange={ setRemindAt }
+            minDate={ new Date() }
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
+          />
+
+          <div className="button" onClick={addReminder} >Add Reminder</div>
+        </div>
+      </div>  
     </div>
   );
 }

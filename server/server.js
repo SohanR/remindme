30794ecbd300b1db71b9be9db89f930cb03740b7
sohanr <mizan.rahman66d@gmail.com>
2022-02-07 16:44:22 +0600

    config DB

--- server/server.js
+++ server/server.js
@@ -5,9 +5,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 
-// configuration of apparent
+// configuration of App
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
+
+
+// configuration of Database
+
+const connectDB = async () => { 
+   await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }, () => console.log("Database Connected"))
+
+}
+
+connectDB();
+
+
+// listen and port 
+const PORT = process.env.PORT || 9000;
+app.listen( PORT, () => console.log(`server is running on port ${PORT}`))
